d8e88c752166311910c9f0179727fc240f692f43
suraj a <surajra0628@gmail.com>
2024-03-27 17:54:40 +0530

    random

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Sling as Hamburger } from 'hamburger-react'
 const Header = () => {
   return (
-    <div className='bg-black text-white flex justify-center items-center h-20 fixed  w-[100vw] overflow-hidden' >
+    <div className='bg-black text-white flex justify-center items-center h-20 absolute  w-[100vw] overflow-hidden' >
         <div className='absolute top-0 left-0 py-7 px-5 xl:px-20 xl:hidden md:hidden'>
          <div  className='absolute top-5 left-2'>
         <Hamburger  size={29} />
